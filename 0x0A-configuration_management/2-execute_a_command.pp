285e022b180a73b2c2a77e499e301c45914e9a72
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2024-02-27 12:17:02 +0100

    Execute a command using puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# Using Puppet to create a manifest that kills a process named killmenow
 exec { 'killmenow':
   command     => '/usr/bin/pkill killmenow',
   path        => ['/usr/bin', '/bin'],  # Add other paths if necessary
