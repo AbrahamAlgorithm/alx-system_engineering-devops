eddbdaf7175e3a7a994e9dd7570851626b6ac86d
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2024-02-27 12:24:37 +0100

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,11 @@
 # Using Puppet to create a manifest that kills a process named killmenow
-exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow',
-  path        => ['/usr/bin', '/bin'],  # Add other paths if necessary
-  refreshonly => true,
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
