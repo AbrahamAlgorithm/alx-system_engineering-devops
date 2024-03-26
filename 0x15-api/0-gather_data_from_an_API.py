448c5adae1e7d6c6b05e5ab912126f56ea9b46a0
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-03-26 01:27:10 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
@@ -19,12 +20,18 @@ def get_employee_todo_progress(employee_id):
         total_tasks = len(todo_data)
         completed_tasks = [task for task in todo_data if task['completed']]
 
-        print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+        print(
+                "Employee {} is done with tasks({}/{})".format(
+                    employee_name, len(completed_tasks), total_tasks
+                    ) + ":"
+                )
+
         for task in completed_tasks:
-            print(f"\t{task['title']}")
+            print("\t{}".format(task["title"]))
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
