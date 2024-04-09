b3e8bf56da94508aa233f565d0ffdf27b7bdf899
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-04-09 22:25:26 +0200

    get subscibers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
+"""get a lit of suscribber on a subreddit"""
 import requests
 
-def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    url = f"https://oauth.reddit.com/r/{subreddit}/about"
-    headers = {"User-Agent": "Mozilla/5.0"}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        return data["data"]["subscribers"]
-    except (requests.exceptions.RequestException, ValueError, KeyError):
-        return 0
+def number_of_subscribers(subreddit):
+    """number of suscribers"""
+    if subreddit is None or type(subreddit) != str:
+        return (0)
+    url = "http://www.reddit.com/r/{}/about.json".format(
+            subreddit)
+    headers = {'User-Agent': 'API project'}
+    response = requests.get(url, headers=headers)
+    if (response.status_code != 200):
+        return (0)
+    sus = response.json().get("data").get('subscribers', 0)
+    return (sus)
