51b6a9c2410f02c8b6dcc829fef9b75e2ef5fa4b
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-04-13 03:54:55 +0200

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
- # solving the wordpress problem
-
-exec{'fix-the-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# A puppet scipt to change a line in a file on a server
+$filetoedit = '/var/www/html/wp-settings.php'
+# Replace the line in the wp-settings.php file containing "phpp" with "php"
+exec { 'fix-wordpress':
+  command => "sed -i 's/phpp/php/g' ${filetoedit}",
+  path    => ['/bin','/usr/bin']
 }
